5ba4504741e8e5b401d4eb493e8114f7f32df60a
Stephen Olson <stephenolson@gmail.com>
2019-03-10 20:43:51 +0000

    Automatically backed up by Learn

---
+++ lib/student.rb
@@ -0,0 +1,30 @@
+class Student
+
+      attr_accessor :name, :location, :profile_url, :profile_quote, :bio, :twitter, :linkedin, :github, :blog
+
+      @@all = []
+
+      def initialize
+        student_hash.each { |key, value| send("#{key}=", value) }
+        @@all << self
+      end
+
+      def create_from_collection(students_array)
+        students_array.each do |student|
+          student_hash= student
+          self.new(student_hash)
+        end
+      end
+
+      def add_student_attributes
+        attributes_hash.each { |key, value| send("#{key}=", value) }
+        @@all << self
+        self
+      end
+
+      def self.all?
+        @@all
+      end
+
+
+end
